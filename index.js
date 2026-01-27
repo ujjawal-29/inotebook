aec404b020a5677bf931edbed6630dafc41649f5
ujjawal <rajputujjawal29@gmail.com>
2026-01-27 19:38:25 +0530

    Move index.js and package.json to root for Render deploy

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-
-
-
 const express = require('express');
-const connectToMongo = require('./db'); 
-var cors = require('cors')
-
-
+const connectToMongo = require('./backend/db'); // <- backend folder में db.js
+var cors = require('cors');
 
 connectToMongo(); // MongoDB connect
 const app = express();
 const port = 5000;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-//Available Routes
-
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/notes',require('./routes/notes'))
+// Available Routes
+app.use('/api/auth', require('./backend/routes/auth')); // <- backend folder में routes
+app.use('/api/notes', require('./backend/routes/notes')); // <- backend folder में routes
 
 app.listen(port, () => {
-    console.log(`iNotebook backend listening at on http://localhost:${port}`);
+    console.log(`iNotebook backend listening at http://localhost:${port}`);
 });
-
-
-
-
