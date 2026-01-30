beb759e8d84c644f4d892717e9bd811ce0c80e11
ujjawal <rajputujjawal29@gmail.com>
2026-01-30 19:08:14 +0530

    Updated backend and frontend files for latest changes

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -1,32 +1,3 @@
-// var jwt =require('jsonwebtoken');
-// const JWT_SECRET ="ujjawalisgoodb&oy"
-
- 
-// const fetchuser = (req,res,next)=>{
-//     // get the user from the jwt token and add id to req object
-
-//     const token = req.header('auth-token');
-//     if(!token){
-//         res.status(401).send({error:"please authenticate using a valid token"})
-//     }
-
-//     try{
-//         const data =jwt.verify(token,JWT_SECRET);
-//     req.user = data.user;
-//     next();
-
-//     }catch(error){
-//        return res.status(401).send({error:"please authenticate using a valid token"})
-
-//     }
-    
-// }
-
-// module.exports = fetchuser
-
-
-
-
 var jwt = require('jsonwebtoken');
 const JWT_SECRET = "ujjawalisgoodb&oy";
 
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const router = express.Router(); 
 var fetchuser = require('../middleware/fetchuser'); 
