63d94a1320431d27ed758e7d19b9682069045ff6
ujjawal <rajputujjawal29@gmail.com>
2026-01-30 12:39:24 +0530

    Fix fetchuser middleware: added return to prevent headers sent error

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -1,25 +1,50 @@
-var jwt =require('jsonwebtoken');
-const JWT_SECRET ="ujjawalisgoodb&oy"
+// var jwt =require('jsonwebtoken');
+// const JWT_SECRET ="ujjawalisgoodb&oy"
 
  
-const fetchuser = (req,res,next)=>{
-    // get the user from the jwt token and add id to req object
+// const fetchuser = (req,res,next)=>{
+//     // get the user from the jwt token and add id to req object
 
-    const token = req.header('auth-token');
-    if(!token){
-        res.status(401).send({error:"please authenticate using a valid token"})
-    }
+//     const token = req.header('auth-token');
+//     if(!token){
+//         res.status(401).send({error:"please authenticate using a valid token"})
+//     }
 
-    try{
-        const data =jwt.verify(token,JWT_SECRET);
-    req.user = data.user;
-    next();
+//     try{
+//         const data =jwt.verify(token,JWT_SECRET);
+//     req.user = data.user;
+//     next();
 
-    }catch(error){
-       return res.status(401).send({error:"please authenticate using a valid token"})
+//     }catch(error){
+//        return res.status(401).send({error:"please authenticate using a valid token"})
 
-    }
+//     }
     
-}
+// }
 
-module.exports = fetchuser
+// module.exports = fetchuser
+
+
+
+
+var jwt = require('jsonwebtoken');
+const JWT_SECRET = "ujjawalisgoodb&oy";
+
+const fetchuser = (req, res, next) => {
+    // get the user from the jwt token and add id to req object
+    const token = req.header('auth-token');
+    if (!token) {
+        return res.status(401).send({ error: "please authenticate using a valid token" });
+        // return zaroori hai taki aage ka code execute na ho
+    }
+
+    try {
+        const data = jwt.verify(token, JWT_SECRET);
+        req.user = data.user;
+        next();
+    } catch (error) {
+        return res.status(401).send({ error: "please authenticate using a valid token" });
+    }
+};
+
+module.exports = fetchuser;
